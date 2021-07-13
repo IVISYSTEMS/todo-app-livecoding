458f0ee2e04d5a3ecdada1808b1531880903133b
h.ryo <h.ryo@ivisystem.com>
2021-07-13 22:55:18 +0900

    ２日目 DB操作を一部実装

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,36 @@
+// パッケージの読み込み
 const express = require('express');
+const sqlite3 = require('sqlite3');
+
+// ルータオブジェクト
 const router = express.Router();
 
-let todos = [];
+// DBオブジェクトの作成
+const db = new sqlite3.Database('db/todo-app-db.db');
 
 /* GET home page. */
+// DBのtodosテーブルから取得したレコードをtodosに渡せればよい
 router.get('/', function(req, res, next) {
-  res.render('index', { todos: todos });
+
+  //DBのテーブルからSELECTを実行
+  db.serialize(() => {
+    db.all("SELECT * FROM todos", (err, rows) => {
+      if(!err) {
+        res.render('index', { todos: rows });
+      }
+    });
+  });  
 });
 
 
 /* POST home page. */
 router.post('/', function(req, res, next) {
-  todos.push(req.body.todo);
-  res.redirect('/');
+  //DBでINSERTする処理に変更したい
+  db.run('INSERT INTO todos(content) VALUES (?)', req.body.todo, (err) => {
+    if(!err) {
+      res.redirect('/');
+    }
+  });
 });
 
 /* GET delete page. */
@@ -31,7 +49,5 @@ router.post('/edit', function(req, res, next) {
   todos[req.query.id] = req.body.todo;
   res.render('index', { todos: todos })
 }); 
-
-
 
 module.exports = router;
